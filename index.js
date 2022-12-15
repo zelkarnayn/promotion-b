8befec2dac2de156bf42c218947947018ef0b159
Magomed <algiraev@MacBook-Air-Magomed.local>
2022-12-15 18:23:27 +0300

    Добавил роуты для магазина

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-require('dotenv').config()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
 
-app.use(cors())
-app.use(express.json())
-mongoose.set('strictQuery', true)
+app.use(cors());
+app.use(express.json());
+mongoose.set("strictQuery", true);
 
-app.use(require("./routes/news.router"))
-app.use(require('./routes/comment.router'))
-
+app.use(require("./routes/news.router"));
+app.use(require("./routes/comment.router"));
+app.use(require("./routes/product.route"));
 
 const server = async () => {
-    try {
-        mongoose.connect(process.env.MONGO_DB, () => {
-            console.log('Успешное подключение к БД');
-        })
-        app.listen(process.env.PORT, () => {
-            console.log('Сервер успешно запущен');
-        })
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+  try {
+    mongoose.connect(process.env.MONGO_DB, () => {
+      console.log("Успешное подключение к БД");
+    });
+    app.listen(process.env.PORT, () => {
+      console.log("Сервер успешно запущен");
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-server()
+server();
