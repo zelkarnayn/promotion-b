a01652f978e27e3689dee37b6869e9eec0758b7a
Zelemhan Idrisov <zelkarnayn@gmail.com>
2022-12-26 01:09:32 +0300

    ДТО

--- controllers/users/dtos/user.dto.js
+++ controllers/users/dtos/user.dto.js
@@ -3,11 +3,13 @@ module.exports = class UserDto {
     id;
     isActivated;
     roles;
+    firstName;
 
     constructor(model) {
         this.email = model.email;
         this.id = model._id;
         this.isActivated = model.isActivated;
         this.roles = model.roles
+        this.firstName = model.firstName
     }
 }
