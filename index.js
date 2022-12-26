d95d13d839b1d2b152908a49f8f39e42f68fd28e
Zelemhan Idrisov <zelkarnayn@gmail.com>
2022-12-26 13:27:21 +0300

    Сборка 1

--- index.js
+++ index.js
@@ -16,15 +16,12 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 app.use("/images", express.static(__dirname + "/images"));
-app.use(require('./routes/categories.route'))
-app.use(require('./routes/fighters.route'))
 mongoose.set('strictQuery', false)
 
-app.use(require("./routes/comment.route"));
-// app.use(require("./routes/product.route"));
-// app.use(require("./routes/categories.route"));
-// app.use(require("./routes/fighters.route"));
-// app.use(require('./routes/auth.route'))
+app.use(require('./routes/product.route'));
+app.use(require("./routes/categories.route"));
+app.use(require("./routes/fighters.route"));
+app.use(require('./routes/auth.route'))
 app.use(require("./routes/news.route"));
 app.use(require("./routes/comment.route"));
 app.use(require("./routes/cart.route"));
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,15 +1,15 @@
-// const { Router } = require("express");
-// const productController = require("../controllers/product.controller");
-// const imageMiddleware = require ('../middleware/post.image')
+const { Router } = require("express");
+const productController = require("../controllers/product.controller");
+const imageMiddleware = require ('../middleware/post.image')
 
 
-// const router = Router();
+const router = Router();
 
-// router.get("/products", productController.getProducts);
-// router.get("/product/:id", productController.getProduct);
-// router.post("/product", productController.createProduct); //imageMiddleware.single("image"), 
-// router.patch("/product/:id", productController.updateProduct);
-// router.delete("/product/:id", productController.deleteProduct);
-// router.post("/images", productController.addProductImage); //imageMiddleware.single("avatar"),
+router.get("/products", productController.getProducts);
+router.get("/product/:id", productController.getProduct);
+router.post("/product", productController.createProduct); //imageMiddleware.single("image"), 
+router.patch("/product/:id", productController.updateProduct);
+router.delete("/product/:id", productController.deleteProduct);
+router.post("/images", productController.addProductImage); //imageMiddleware.single("avatar"),
 
-// module.exports = router;
+module.exports = router;
